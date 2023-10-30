bd52ada8df0c0c2fb3899dbef18978b011bb79b4
nixtomalon <nonixtomalon@gmail.com>
2023-10-30 23:36:59 +0800

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [points, setPoints] = useState([]);
+  const [popPoints, setPopPoints] = useState([]);
+  
+  const handleOnclick = (e) => {
+    const { clientX, clientY } = e;
+    setPoints([...points, {
+      x: clientX,
+      y: clientY
+    }]);
+  }
+
+  const handleUndo = () => {
+    const pointsList = [...points];
+    const popped = pointsList.pop();
+    setPopPoints([...popPoints, popped]);
+    setPoints(pointsList);
+  }
+
+  const handleRedo = () => {
+    const pointsList = [...popPoints];
+    const popped = pointsList.pop();
+    setPoints([...points, popped]);
+    setPopPoints(pointsList);
+  }
+
+  return (
+    <>
+    <button onClick={handleUndo}>undo</button>
+    <button onClick={handleRedo}>Redo</button>
+      <div className="app" onClick={handleOnclick}>
+        {points.map((point, idx) => 
+          <div key={idx} className="circle" style={{
+            left: point.x + "px",
+            top: point.y + "px",
+          }}></div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
